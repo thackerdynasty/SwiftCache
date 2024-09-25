adc41be14ee4bf44714e0a8a8a97b87beed62815
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2024-09-25 18:33:26 -0500

    Add docs

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "SwiftCache",
             targets: ["SwiftCache"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", .upToNextMajor(from: "1.4.3"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
